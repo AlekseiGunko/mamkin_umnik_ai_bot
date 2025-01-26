719c810c0e656f01f0bcebe6c0efc31e3a1db5c3
Aleksei Gunko <igrokk32rus@yandex.ru>
2025-01-26 17:40:30 +0300

    create second block - generations images optimazing code - refactor work on third block - text + images. Block have exceptions and errors - need fix

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,7 +1,15 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Чат')]
+    [KeyboardButton(text='Чат')],
+    [KeyboardButton(text='Генерация картинок')]
 ],
                             resize_keyboard=True,
                             input_field_placeholder='Выберите пункт меню')
+
+
+cancel = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='Отмена')]
+],
+                            resize_keyboard=True,
+                            input_field_placeholder='Закончить диалог')
--- run.py
+++ run.py
@@ -4,15 +4,22 @@ from aiogram import Bot, Dispatcher
 
 from config import TOKEN
 
-from app.user import user
+from app.user import user_r
 from app.admin import admin
+
+from app.database.models import async_main
 
 
 async def main():
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
-    dp.include_routers(user, admin)
+    dp.include_routers(user_r, admin)
+    dp.startup.register(on_startup)
     await dp.start_polling(bot)
+
+
+async def on_startup(dispatcher):
+    await async_main()
 
 
 if __name__ == '__main__':
